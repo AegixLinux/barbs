d3fdcd94235ac68c5ea943bf1405b8d39089fcdc
Tank Linux <tanklinux@protonmail.com>
2023-07-27 16:28:21 -0400

    07/27/23 16:28:21

--- barbs.sh
+++ barbs.sh
@@ -467,7 +467,7 @@ pre_install_message() {
 # Note: This function assumes that the script has completed successfully.
 finale() {
 	whiptail --title "All done!" \
-		--msgbox "Congrats! You're done.\\n\\nIf you came from tl.sh, you'll return there to reboot into your nice, new system.\\n\\nIf you ran BARBS standalone, you can login as the your new user and type: startx to launch the new graphical environment. Logging in after reboot will land you in TTY1 as default which will preclude the need to run startx\\n\\nMucho Amor\\n-TANKLINUX.COM" 16 90
+		--msgbox "Congrats! You're done with BARBS.\\n\\nIf you came from tl.sh, you'll return there to reboot into your nice, new system.\\n\\nIf you ran BARBS standalone, you can login as your new user and type: startx to launch the new graphical environment. Logging in after reboot will land you in tty1 as default which will preclude the need to run startx\\n\\nMucho Amor\\n-TANKLINUX.COM" 16 90
 }
 
 ################################
