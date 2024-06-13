40069dceea47508aabc473327448487d113f0dce
AegixLinux <aegixlinux@proton.me>
2024-06-13 17:02:04 -0400

    06/13/24 17:02:04

--- barbs.sh
+++ barbs.sh
@@ -452,7 +452,7 @@ pre_install_message() {
 # This function assumes that the script has completed successfully.
 finale() {
 	whiptail --title "All done!" \
-		--msgbox "Congrats! You're done with BARBS.\\n\\nIf you got here the traditinoal route from install.sh, you'll be returned to your nice, new system.\\n\\nIf you ran BARBS standalone, you can run startx as your new user. Logging in after reboot will land you in tty1 which will auto-run startx\\n\\nEnjoy\\nAegix" 16 90
+		--msgbox "Congrats! You're done with BARBS.\\n\\nIf you got here the traditional route from install.sh, you'll be returned to your nice, new system.\\n\\nIf you ran BARBS standalone, you can run startx as your new user. Logging in after reboot will land you in tty1 which will auto-run startx\\n\\nEnjoy\\nAegix" 16 90
 }
 
 ###
